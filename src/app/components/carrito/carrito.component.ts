9de5966a358fafbce714fa34a1d9efc9ff305390
sergioo267 <sesa.lu@hotmail.es>
2020-12-17 11:09:14 +0100

    web casi acabada

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Producto } from 'src/app/model/producto';
 import { Seguro } from 'src/app/model/seguro';
@@ -10,33 +10,54 @@ import { CompraProductosService } from 'src/app/services/compra-productos.servic
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
 })
-export class CarritoComponent implements OnInit {
+export class CarritoComponent implements OnInit , OnChanges{
   productos!:Producto[];
   seleccionado= "true";
   show:boolean =false;
-
+  seguros:Seguro[] = new Array();
+  compra!:boolean;
+  compraFinalizada:boolean = false;
+  codigoTienda:number = 28832;
   constructor(private carritoServices:CarritooService, private compraServices:CompraProductosService, private router:Router ) { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.compraFinalizada == true){
+      this.compraServices.addProductosComprado(this.productos);
+      this.carritoServices.borrar();
+      this.router.navigate(["devoluciones-pedidos"])
+    }
+  }
+
+  comprado(valor:any){
+    console.log("fsdfs")
+    this.compraFinalizada = true;
+  }
+
+
   ngOnInit(): void {
     this.productos = this.carritoServices.getProductos();
     if (this.productos.length > 0 ){
       this.show = true;
     }
     console.log(this.productos)
-    this.productos.forEach(elements=> {
-      if( elements instanceof Producto){
-        console.log("producto")
-      }else{
-        console.log("seguro")
+    this.productos.forEach(element=> {
+      if( element instanceof Producto){
 
+      }else{
+        this.seguros.push(element)
       }
     })
   }
 
   comprar(){
-    this.compraServices.addProductosComprado(this.productos);
-    this.carritoServices.borrar();
-    this.router.navigate(["devoluciones-pedidos"])
+    this.compra = true;
+
+    // this.compraServices.addProductosComprado(this.productos);
+    // this.carritoServices.borrar();
+    // this.router.navigate(["devoluciones-pedidos"])
 
   }
+
+
 
 }
